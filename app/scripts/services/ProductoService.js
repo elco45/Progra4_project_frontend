c6bb9080204f3895401ecb73c6ebb9a7c23dddcb
jossymelgar <jossy.xavier@unitec.edu>
2015-12-14 19:33:07 -0600

    bienvenida

--- app/scripts/services/ProductoService.js
+++ app/scripts/services/ProductoService.js
@@ -1,7 +1,7 @@
 angular.module('AngularScaffold.Services').factory('ProductoService', ['$http',
 	function($http){
 		$http.defaults.withCredentials = true;
-		var baseUrl = 'https://bodega-emelina-backend.herokuapp.com/';
+		var baseUrl = 'http://localhost:8000/';
 		return {
 			GetProductos:function(){
 				return $http.get(baseUrl + "v1/productos");
--- app/scripts/services/UserService.js
+++ app/scripts/services/UserService.js
@@ -1,7 +1,7 @@
 angular.module('AngularScaffold.Services').factory('UserService', ['$http',
 	function($http){
 		$http.defaults.withCredentials = true;
-		var baseUrl = 'https://bodega-emelina-backend.herokuapp.com/';
+		var baseUrl = 'http://localhost:8000/';
 		return {
 			Register: function(payload){
 	          return $http.post(baseUrl + "v1/register", payload);
