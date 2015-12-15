486b06fd969a2d4d6ba678ff7234c4ba7ffeb59c
elco45 <dennischeong2003@yahoo.es>
2015-12-14 21:08:26 -0600

    actualizar tabla

--- app/scripts/services/ProductoService.js
+++ app/scripts/services/ProductoService.js
@@ -1,7 +1,7 @@
 angular.module('AngularScaffold.Services').factory('ProductoService', ['$http',
 	function($http){
 		$http.defaults.withCredentials = true;
-		var baseUrl = 'https://bodega-emelina-backend.herokuapp.com/';
+		var baseUrl = 'http://localhost:8000/';
 		return {
 			GetProductos:function(){
 				return $http.get(baseUrl + "v1/productos");
@@ -9,10 +9,10 @@ angular.module('AngularScaffold.Services').factory('ProductoService', ['$http',
 			PostProductos: function(payload){
 				return $http.post(baseUrl + "v1/productos",payload);
 			},
-		    PutProductos: function(payload){
+		    PutProductos: function(payload,w){
 				return $http.put(baseUrl+"v1/modprod",payload);
 			},
-			DelProductos: function(id){
+			DelProductos: function(id,a){
 				return $http.delete(baseUrl+"v1/delprod/"+id);
 			}
 	    };
--- app/scripts/services/UserService.js
+++ app/scripts/services/UserService.js
@@ -1,15 +1,15 @@
 angular.module('AngularScaffold.Services').factory('UserService', ['$http',
 	function($http){
 		$http.defaults.withCredentials = true;
-		var baseUrl = 'https://bodega-emelina-backend.herokuapp.com/';
+		var baseUrl = 'http://localhost:8000/';
 		return {
-			Register: function(payload){
+			Register: function(payload,w){
 	          return $http.post(baseUrl + "v1/register", payload);
 	        },
 	        GetUsers:function(){
 				return $http.get(baseUrl + "v1/users");
 			},
-			DelUsers: function(username){
+			DelUsers: function(username,w){
 				return $http.delete(baseUrl+"v1/deluser/"+username);
 			}
 	    };
